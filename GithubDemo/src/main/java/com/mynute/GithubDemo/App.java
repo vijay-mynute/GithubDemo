68b3c30d6861c40c87e36993fc3182208884c188
SAMSUNG <SAMSUNG@SAMSUNG-PC>
2020-11-28 22:27:14 +0530

    commit all files

---
+++ GithubDemo/src/main/java/com/mynute/GithubDemo/App.java
@@ -0,0 +1,13 @@
+package com.mynute.GithubDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
